7d96bea68a1be4ce6aed8bdf1cc0164aa2390958
LubyRuffy <lubyruffy@gmail.com>
2018-01-16 00:53:50 +0800

    add armv5 compile

--- build.bat
+++ build.bat
@@ -10,11 +10,22 @@ call :build freebsd 386
 call :build freebsd amd64
 call :build windows 386 .exe
 call :build windows amd64 .exe
+call :armv5
 goto :end
 
 :build
   mkdir .\%1\%2
-	set GOOS=%1
-	set GOARCH=%2
-	go build -o %1/%2/tcptunnel%3 -i -ldflags "-w -s" .
+  set GOOS=%1
+  set GOARCH=%2
+  go build -o %1/%2/tcptunnel%3 -i -ldflags "-w -s" .
+  goto :eof
+
+:armv5
+  mkdir .\linux\armv5
+  set GOOS=linux
+  set GOARCH=arm
+  set GOARM=5
+  go build -o linux/armv5/tcptunnel -i -ldflags "-w -s" .
+  goto :eof
+
 :end
